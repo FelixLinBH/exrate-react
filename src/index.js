fc331d266ca8e0c27737569468e522404233d0a2
Felix.lin <fly_81211@hotmail.com>
2018-01-01 23:53:35 +0800

    Added support lib.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-table/dist/bootstrap-table.min.css';
+window.$ = window.jQuery = require('jquery')
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
