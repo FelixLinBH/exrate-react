a9189864ec5d8135ae50ba6102137bc7e46e01ad
felix.lin <fly_81211@hotmail.com>
2018-01-03 01:06:58 +0800

    Added vendor.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-table/dist/bootstrap-table.min.css';
+import './vendor/fresh-bootstrap-table.css';
+import './vendor/fonts.css';
+import './vendor/from_style.css';
+import './vendor/grayscale.min.css';
+import './vendor/normalize.min.css';
+import './vendor/bootstrap-select.min.css';
 window.$ = window.jQuery = require('jquery')
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
